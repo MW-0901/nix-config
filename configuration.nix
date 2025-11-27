90e0ffafa3ff6175894e8e5bf7b5796f2e43cf6e
Mark Wilson <MW0901@proton.me>
2025-11-26 22:57:05 -0800

    Fix flake configuration

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,8 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running 'nixos-help').
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, globalPackages, ... }:
 
 {
   imports =
@@ -15,8 +15,8 @@
 
   security.pam.u2f = {
     enable = true;
-    settings.cue = true;       # Makes it show a “Touch your YubiKey” prompt
-    control = "sufficient";  # Block access if U2F fails
+    settings.cue = true;       # Makes it show a "Touch your YubiKey" prompt
+    control = "sufficient";  # Try U2F first, skip password if successful
   };
 
   security.pam.services.doas.u2fAuth = true;
@@ -89,7 +89,7 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.mark = {
     isNormalUser = true;
     description = "Mark Wilson";
@@ -110,11 +110,9 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = let
-  globalpkgs = (builtins.getFlake "path:/home/mark/config").packages.${pkgs.system}.default;
-   in [
-    globalpkgs
-   ];
+  environment.systemPackages = [
+    globalPackages
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -137,7 +135,7 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
--- flake.nix
+++ flake.nix
@@ -1,28 +1,58 @@
 {
-  description = "All my global nixos packages :)";
+  description = "NixOS system configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs { 
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
-      # default attribute is a list of packages
+      # Your global packages
       packages.${system}.default = pkgs.buildEnv {
         name = "global-packages";
         paths = [
           pkgs.firefox
           pkgs.git
           pkgs.curl
-	  pkgs.wget
-	  pkgs.gnupg
-	  pkgs.pinentry-qt
-	  pkgs.neovim
-	  pkgs.gh
-	  pkgs.doas
+          pkgs.wget
+          pkgs.gnupg
+          pkgs.pinentry-qt
+          pkgs.neovim
+          pkgs.gh
+          pkgs.doas
+          pkgs.direnv
+          pkgs.nix-direnv
+          pkgs.home-manager
+          pkgs.fd
+          pkgs.tree
+        ];
+      };
+
+      # Your NixOS system
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { 
+          globalPackages = self.packages.${system}.default; 
+        };
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mark = import ./home.nix;
+          }
         ];
       };
     };

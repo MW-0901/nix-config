68282a7c352b534b5f22d42063d2d717040a3851
Mark Wilson <MW0901@proton.me>
2025-11-26 21:59:47 -0800

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "All my global nixos packages :)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      # default attribute is a list of packages
+      packages.${system}.default = pkgs.buildEnv {
+        name = "global-packages";
+        paths = [
+          pkgs.firefox
+          pkgs.git
+          pkgs.curl
+	  pkgs.wget
+	  pkgs.gnupg
+	  pkgs.pinentry-qt
+	  pkgs.neovim
+        ];
+      };
+    };
+}

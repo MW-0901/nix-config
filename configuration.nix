4176fa15c7f15705e7641fd02eba7aae70ec1ffb
Mark Wilson <MW0901@proton.me>
2025-12-01 19:39:40 -0800

    Switched back over to nm w/ iwd backend

--- configuration.nix
+++ configuration.nix
@@ -43,17 +43,8 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = false;
-  networking.wireless.iwd.enable = true;
-  networking.wireless.iwd.settings = {
-    Network = {
-      EnableIPv6 = true;
-    };
-    Settings = {
-      AutoConnect = true;
-    };
-  };
-
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
